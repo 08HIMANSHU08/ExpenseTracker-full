bcfcc58a3cdb27436b052c3c1e9e66d9e51095f6
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-21 11:10:23 +0530

    backend ready

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ app.use('/password',passwardRoutes);
 
 app.use(errorController.get404)
 
-
 // app.use((req,res)=>{
 //     // console.log(req.url);
 //     res.sendFile(path.join(__dirname,`public/${req.url}`));
